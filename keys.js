037d645be46067525b30552944cf224af0f1560a
Alfredo Munoz <AlfredoM@Alfredos-MacBook-Pro.local>
2017-10-11 20:45:51 -0400

    Added Keys and additional npm packages

---
+++ keys.js
@@ -0,0 +1,14 @@
+var twitterKey = {
+    consumer_key: 'Hwac4QYYklSiLGqB1NO9uz6gV',
+    consumer_secret: 'UWQtDxZ1c9ASV6RJBw0JAkR0pyjzBxx6fIqsD1NOLwxA8EhE0t',
+    access_token_key: '479916432-G93Kxkr2GwZnDbkfWCIVf97hG7oHT4fzkMM74JIw',
+    access_token_secret: 'nnBtrpU4AZDcTtzlQNUjeLYDQMxyHx8gIjDpgWAWMhgCs',
+}
+
+var spotifyKey = {
+    id: '004021a76b634828a5d673645ac16dec',
+    secret: 'a398a129d043471caf59729e95b2270c'
+}
+
+exports.twitterKeys = twitterKey;
+exports.spotifyKeys = spotifyKey;
